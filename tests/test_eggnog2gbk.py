1c45e1f22a3fe3fe996c18bbebbb63cb74cbd412
cfrioux <clemence.frioux@gmail.com>
2020-05-19 09:51:55 +0200

    test genomic mode no gff

--- tests/test_eggnog2gbk.py
+++ tests/test_eggnog2gbk.py
@@ -2,7 +2,6 @@
 
 """Tests for `eggnog2gbk` package."""
 
-import pytest
 import os
 import shutil
 import subprocess
@@ -16,31 +15,98 @@ FNA_INPUT = "betaox.fna"
 ANNOT_INPUT = "betaox_annotation.tsv"
 ORG_NAME = "Escherichia coli"
 
-EXPECTED_GBK = "betaox_from_gff.gbk"
-GBK_OUTPUT = "test.gbk"
+EXPECTED_GBK = "betaox_no_gff.gbk"
+
+ANNOTATIONS_TYPES = ['go_function', 'go_process', 'EC_number']
+
+def gbk_no_gff_test():
+    """Test genomic mode without gff as input.
+    """
+    gbk_test = 'test_no_gff.gbk'
+    gbk_creation(genome=FNA_INPUT,
+                proteome=FAA_INPUT,
+                annot=ANNOT_INPUT,
+                org=ORG_NAME,
+                gbk=gbk_test,
+                gff=None,
+                gobasic='go-basic.obo')
+
+    loaded_gbk = SeqIO.to_dict(SeqIO.parse(EXPECTED_GBK, "genbank"))
+    loaded_test = SeqIO.to_dict(SeqIO.parse(gbk_test, "genbank"))
+
+    assert set(loaded_gbk.keys()) == set(loaded_test.keys())
+
+    # annotations_expected = {i:loaded_gbk[i].features[e].qualifiers 
+    #                             if loaded_gbk[i].features[e].type=='CDS' 
+    #                             else None 
+    #                             for e in range(0, len(loaded_gbk[i].features)) 
+    #                         for i in loaded_gbk.keys()}
+    
+    # annotations_test = {i:loaded_test[i].features[e].qualifiers 
+    #                             if loaded_test[i].features[e].type=='CDS' 
+    #                             else None 
+    #                             for e in range(0, len(loaded_test[i].features)) 
+    #                         for i in loaded_test.keys()}
+
+    annotations_expected = {i:None 
+                            for i in loaded_gbk.keys()}
+    
+    annotations_test = {i:None 
+                        for i in loaded_test.keys()}
+
+    for gene in annotations_expected: 
+        for index in range(0, len(loaded_gbk[gene].features)):
+            if loaded_gbk[gene].features[index].type=='CDS':
+                annotations_expected[gene] = loaded_gbk[gene].features[index].qualifiers
+
+    for gene in annotations_test: 
+        for index in range(0, len(loaded_test[gene].features)):
+            if loaded_test[gene].features[index].type=='CDS':
+                annotations_test[gene] = loaded_test[gene].features[index].qualifiers
+
+    for gene in annotations_expected:
+        for qualifier in annotations_expected[gene]:
+            if qualifier in ANNOTATIONS_TYPES:
+                assert set(annotations_expected[gene][qualifier]) == set(annotations_test[gene][qualifier])
+
+    os.remove(gbk_test)
+    return
+
 
 def gbk_from_gff_test():
-    """Test with a gff as input.
+    """Test genomic mode with a gff as input.
     """
+    gbk_test = 'test_gff.gbk'
+
     gbk_creation(genome=FNA_INPUT,
                 proteome=FAA_INPUT,
                 annot=ANNOT_INPUT,
                 gff=GFF_INPUT,
                 org=ORG_NAME,
-                gbk=GBK_OUTPUT)
+                gbk=gbk_test,
+                gobasic='go-basic.obo')
 
-    loaded_gbk = SeqIO.parse(EXPECTED_GBK, "genbank")
-    loaded_test = SeqIO.parse(GBK_OUTPUT, "genbank")
+    loaded_gbk = SeqIO.to_dict(SeqIO.parse(EXPECTED_GBK, "genbank"))
+    loaded_test = SeqIO.to_dict(SeqIO.parse(gbk_test, "genbank"))
 
-    #TODO finish
-
+    os.remove(gbk_test)
+    #TODO
     return
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
 
-
-def gbk_no_gff_test():
-    """Test without a gff as input.
+def gbk_from_dir_test():
+    """Test genomic mode with directories.
     """
     #TODO
     return
+
+def gbk_metagenomic_mode_test():
+    """Test metagenomic mode.
+    """
+    #TODO
+    return
+    
+if __name__ == "__main__":
+    gbk_no_gff_test()
+    # gbk_from_gff_test()
+    # gbk_from_dir_test()
+    # gbk_metagenomic_mode_test()
