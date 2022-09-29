9698257acce2c065436abfe6f699772b6138a461
Arnaud Belcour <arnaud.belcour@irisa.fr>
2022-09-29 14:17:13 +0200

    Fix issue with pypi classifiers.

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: LGPLv3+',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
         'Natural Language :: English',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
